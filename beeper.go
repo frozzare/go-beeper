91c15c86bf94fa1434d8937dd2484e0ba45b8b95
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-12-04 21:07:53 +0100

    Go fmt

--- beeper.go
+++ beeper.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-const delay = 100;
+const delay = 100
 
 // Just beep!
 func beep() {
@@ -13,37 +13,37 @@ func beep() {
 }
 
 // Beep a melody.
-func Melody(params ...string, ) bool {
+func Melody(params ...string) bool {
 	if len(params) == 0 {
 		return false
 	}
-	
+
 	for _, note := range params[0] {
 		if note == 42 {
 			beep()
 		}
-		
+
 		time.Sleep(time.Second + delay)
 	}
-	
+
 	return true
 }
 
 // Beep numbers of times.
 func Beep(params ...int) bool {
-	if (len(params) == 0) {
+	if len(params) == 0 {
 		params = make([]int, 1)
 		params[0] = 1
 	}
-	
+
 	for _, num := range params {
 		for i := 0; i < num; i++ {
 			beep()
-			if i + 1 < num {
+			if i+1 < num {
 				time.Sleep(time.Second + delay)
 			}
 		}
 	}
-	
+
 	return true
 }
--- beeper_test.go
+++ beeper_test.go
@@ -1,21 +1,21 @@
 package beeper
 
-import(
-	"testing"
+import (
 	"github.com/bmizerany/assert"
+	"testing"
 )
 
 // Beep one time
-func TestBeepOneTime (t *testing.T) {
+func TestBeepOneTime(t *testing.T) {
 	assert.Equal(t, Beep(), true)
 }
 
 // Beep three times
-func TestBeepThreeTimes (t *testing.T) {
+func TestBeepThreeTimes(t *testing.T) {
 	assert.Equal(t, Beep(3), true)
 }
 
 // Beep a melody
-func TestBeepMelodicTimes (t *testing.T) {
+func TestBeepMelodicTimes(t *testing.T) {
 	assert.Equal(t, Melody("****-*-*"), true)
 }
